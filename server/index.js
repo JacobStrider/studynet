c0e7acf5a9d3a6db6d043224c2ae3b9d83ee2578
Jacob <Jacob.strider0@gmail.com>
2026-04-22 23:23:06 -0400

    Initial commit - StudyNet MVP

---
+++ server/index.js
@@ -0,0 +1,47 @@
+require("dotenv").config();
+const pool = require("./database");
+
+
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const PORT = 5000;
+
+// Test
+app.get("/", (req, res) => {
+    res.send("Hello World!");
+});
+
+// GET notes (from database)
+app.get("/notes", async (req, res) => {
+  try {
+    const result = await pool.query("SELECT * FROM notes ORDER BY id DESC");
+    res.json(result.rows);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "Server error" });
+  }
+});
+
+// POST note (to database)
+app.post("/notes", async (req, res) => {
+  try {
+    const { title, content } = req.body;
+    const result = await pool.query(
+      "INSERT INTO notes (title, content) VALUES ($1, $2) RETURNING *",
+      [title, content]
+    );
+    res.json(result.rows[0]);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "Server error" });
+  }
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
